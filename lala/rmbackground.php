9b5a681274fa15cb7e1fc7e68459e6bf134833d0
13utters <42936468+13utters@users.noreply.github.com>
2018-09-10 02:07:19 -0500

    Update rmbackground.php

--- lala/rmbackground.php
+++ lala/rmbackground.php
@@ -1,14 +1,12 @@
 <?php
-header('Access-Control-Allow-Origin: https://plug.dj';); 
-header('Content-Type: text/json');
-
-$m1 = "https://cdn.rawgit.com/13utters/MM-Master/ed7aae0f/lala/mm.json";
-$m2 = "https://cdn.rawgit.com/13utters/MM-Master/8ebfd549/lala/mm.json";
-$m3 = "https://cdn.rawgit.com/13utters/MM-Master/68199e70/lala/mm.json";
-$m4 = "https://cdn.rawgit.com/13utters/MM-Master/3351e36a/lala/mm.json";
-$m5 = "https://cdn.rawgit.com/13utters/MM-Master/a6378258/lala/mm.json";
-$m6 = "https://cdn.rawgit.com/13utters/MM-Master/052d1890/lala/mm.json";
-
+header('Access-Control-Allow-Origin: https://plug.dj'); 
+header('Content-Type: text/json'); 
+$m1 = "https://cdn.radiant.dj/rcs/bg/2k14tc.jpg";
+$m2 = "https://cdn.radiant.dj/rcs/bg/2k14hw.jpg";
+$m3 = "https://cdn.radiant.dj/rcs/bg/2k14tiki.jpg";
+$m4 = "https://cdn.radiant.dj/rcs/bg/2k14worldonfire.jpg";
+$m5 = "https://cdn.radiant.dj/rcs/bg/5.png";
+$m6 = "https://cdn.radiant.dj/rcs/bg/7.png";
 $rm = array($m1, $m2, $m3, $m4, $m5, $m6);
 $random_keys = array_rand($rm, 1);
 echo $rm[$random_keys];
